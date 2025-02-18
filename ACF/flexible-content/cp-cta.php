2f2b5c31cf41f44f79c6dc8f71358f6839a9bcef
aynat <aynatcm@gmail.com>
2025-02-18 02:29:47 -0600

    cp cta done

---
+++ ACF/flexible-content/cp-cta.php
@@ -0,0 +1,23 @@
+<?php
+$titleSection = $cp->title_section;
+$imageSection = $cp->image_section;
+
+?>
+
+<section class="container--cta">
+    <div class="container--wrapper">
+        <div class="container--image-cta">
+            <img src="<?php echo $imageSection ?>" alt="image section" class="image--cta">
+
+            <div class="container--overlay-cta">
+
+            </div>
+        </div>
+
+
+        <div class="container--title-cta">
+            <h4 class="title--cta"><?php echo $titleSection ?></h4>
+        </div>
+
+    </div>
+</section>
